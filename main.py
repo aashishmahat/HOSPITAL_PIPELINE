0fc5d9666b07aa124327943e51f1c3216cb113c5
aashishmahat <080bct004@ioepc.edu.np>
2026-03-25 12:34:10 +0545

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pipeline.extract import load_datasets
 from pipeline.transform import transforms_data
-from pipeline.load import store
+from pipeline.load import Storage_Choice
 from report.reportgeneration import patient_report
 from report.reportgeneration import doctor_patient
 from Logs.logger_setup import get_logger
@@ -12,7 +12,7 @@ def run_pipeline():
     logger.info("=== Pipeline Started ===")
     datasets     = load_datasets()
     cleaned_data = transforms_data(datasets)
-    store(cleaned_data)
+    Storage_Choice(cleaned_data)
     patient_report(cleaned_data)
     doctor_patient(cleaned_data)
     logger.info("=== Pipeline Completed ===")
